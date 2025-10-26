086e35b768e8a3d9d3e64f7610bb2698b2241029
ZdasienStefano <styzdasien@gmail.com>
2025-10-26 16:00:51 -0300

    Creacion de AccesoDatos, ProveedorNegocio, ProductoNegocio(vacio y en BD la tabla Proveedor)

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return Lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=TPCuatri_DB ; integrated security=true");
+            comando = new SqlCommand();
+
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        public object ejecutarScalar()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                if (conexion.State != System.Data.ConnectionState.Open)
+                    conexion.Open();
+
+                return comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+
+
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+
+            comando.Parameters.AddWithValue(nombre, valor);
+
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+
+        }
+
+    }
+
+}
---
+++ Negocio/ProveedorNegocio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ProveedorNegocio
+    {
+        //public List<Producto> listar()
+        //{
+        //    List<Producto> lista = new List<Producto>();
+        //    AccesoDatos datos = new AccesoDatos();
+
+        //    try
+        //    {
+        //        datos.setearConsulta("select RazonSocial, Nombre, Cuit, Telefono, Email, Direccion, Localidad from Proveedores");
+        //        datos.ejecutarLectura();
+
+        //        while (datos.Lector.Read())
+        //        {
+        //            Producto aux = new Producto();
+        //            aux.IdMarca = (int)datos.Lector["Id"];
+        //            aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+        //            lista.Add(aux);
+        //        }
+
+
+        //        return lista;
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //    finally
+        //    {
+        //        datos.cerrarConexion();
+        //    }
+
+        //}
+
+    }
+}
